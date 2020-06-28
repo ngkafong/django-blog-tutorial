ccc76b2a0860d1fa633003453307f141697d81ae
Ng Ka Fong <richardng1126@gmail.com>
2020-06-28 14:39:33 +0800

    user profile modification form

--- blog/url.py
+++ blog/url.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -11,6 +12,19 @@ def home(request):
 
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    # template_name = <app>/<model>_<viewtype>.html
+
+
 
 
 def about(request):
