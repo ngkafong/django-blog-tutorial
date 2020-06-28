dd03123690635b83e08356a558b06d4893287d14
Ng Ka Fong <richardng1126@gmail.com>
2020-06-28 21:50:18 +0800

    post create form

--- blog/url.py
+++ blog/url.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import (
+    ListView, DetailView, CreateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -25,6 +27,13 @@ class PostDetailView(DetailView):
     # template_name = <app>/<model>_<viewtype>.html
 
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    # template_name = <app>/<model>_form.html
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
